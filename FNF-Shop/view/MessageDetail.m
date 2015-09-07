ba5b711c698f83f1b4f055705459e7c4dc492481
公共开发 <pubdev@users-iMac-3.local>
2015-09-07 21:39:34 +0800

    0907

--- FNF-Shop/view/MessageDetail.m
+++ FNF-Shop/view/MessageDetail.m
@@ -7,17 +7,22 @@
 //
 
 #import "MessageDetail.h"
+#import "YcKeyBoardView.h"
 
+#define kWinSize [UIScreen mainScreen].bounds.size
 #define TOOLBARTAG		200
 #define TABLEVIEWTAG	300
 
-@interface MessageDetail ()
+@interface MessageDetail () <YcKeyBoardViewDelegate>
 {
     CGRect viewOrigin;
 }
+
+@property (nonatomic,strong)YcKeyBoardView *key;
 @property (weak, nonatomic) IBOutlet UITextField *inputLabel;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet TableViewWithBlock *tableView;
+@property (weak, nonatomic) IBOutlet UITextField *inputTextVIew;
 @end
 
 @implementation MessageDetail
@@ -50,6 +55,49 @@
 {
     [AppDelegate jumpToMsgList];
 }
+- (IBAction)OnClickInput:(id)sender {
+}
+
+-(void)addBtn:(UIButton *)btn
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardHide:) name:UIKeyboardWillHideNotification object:nil];
+    if(self.key==nil){
+        self.key = [[YcKeyBoardView alloc] initWithFrame:CGRectMake(0, kWinSize.height-44, kWinSize.width, 44)];
+    }
+    self.key.delegate = self;
+    [self.key.textView becomeFirstResponder];
+    self.key.textView.returnKeyType=UIReturnKeySend;
+    [self.view addSubview:self.key];
+}
+-(void)keyboardShow:(NSNotification *)note
+{
+    CGRect keyBoardRect=[note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    CGFloat deltaY=keyBoardRect.size.height;
+    [UIView animateWithDuration:[note.userInfo[UIKeyboardAnimationDurationUserInfoKey] floatValue] animations:^{
+        
+        self.key.transform=CGAffineTransformMakeTranslation(0, -deltaY);
+    }];
+}
+-(void)keyboardHide:(NSNotification *)note
+{
+    [UIView animateWithDuration:[note.userInfo[UIKeyboardAnimationDurationUserInfoKey] floatValue] animations:^{
+        
+        self.key.transform=CGAffineTransformIdentity;
+    } completion:^(BOOL finished) {
+        
+        self.key.textView.text=@"";
+        [self.key removeFromSuperview];
+    }];
+    
+}
+-(void)keyBoardViewHide:(YcKeyBoardView *)keyBoardView textView:(UITextView *)contentView
+{
+    [contentView resignFirstResponder];
+    //接口请求
+    [self.inputText setText:[contentView text]];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
